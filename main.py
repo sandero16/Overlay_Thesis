f8950d7368ddaf71f2d4695c37ccb9951d37ff7f
Pieterjan Luyssen <pieterjan.luyssen@tomtom.com>
2019-11-02 15:32:05 +0100

    automated

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-import SimpleITK as sitk
-import matplotlib.pyplot as plt
-import numpy as np
 import os
-from PIL import Image
-
-#Important part for overlay!!!
-def overlay(fixedImageString, movingImageString, resultImageString):
-    os.system('magick composite -blend 30 ' + fixedImageString + ' ' +movingImageString + ' ' +'merged_original'+movingImageString)
 
 
-fixedImageString = "town3.jpeg"
-movingImageString = "town3.bmp"
-resultImageString = "result.png"
-overlay(fixedImageString, movingImageString, resultImageString)
+def overlay(background_path, foreground_path, result_path):
+    os.system(
+        'magick composite -blend 30 ' + foreground_path + ' ' + background_path + ' ' + result_path)
 
 
+if __name__ == "__main__":
+    """
+    Need 3 dirs:
+    - background
+    - foreground
+    - results
+    """
+    file_list = os.listdir('./foreground')
+    foreground_number_files = len(os.listdir('./foreground'))
+    background_number_files = len(os.listdir('./background'))
+
+    assert foreground_number_files == background_number_files, "foreground and background dir contains not a equal files"
+
+    for file in file_list:
+        name_parts = file.split('.')
+        background_path = './background/' + name_parts[0] + '.png'
+        foreground_path = './foreground/' + name_parts[0] + '.bmp'
+        result_path = './results/' + name_parts[0] + '.png'
+        overlay(background_path, foreground_path, result_path)
+        print('Written '+ name_parts[0] +' to '+ result_path)
 
