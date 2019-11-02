3bb049ca9a188ef90d1c96658e8d95ba66a3a3e8
Pieterjan Luyssen <pieterjan.luyssen@tomtom.com>
2019-11-02 15:40:04 +0100

    automated 2.0

--- main.py
+++ main.py
@@ -5,19 +5,17 @@ def overlay(background_path, foreground_path, result_path):
     os.system(
         'magick composite -blend 30 ' + foreground_path + ' ' + background_path + ' ' + result_path)
 
-
-if __name__ == "__main__":
+def main():
     """
-    Need 3 dirs:
-    - background
-    - foreground
-    - results
-    """
+        Need 3 dirs:
+        - background
+        - foreground
+        - results
+        """
     file_list = os.listdir('./foreground')
-    foreground_number_files = len(os.listdir('./foreground'))
-    background_number_files = len(os.listdir('./background'))
 
-    assert foreground_number_files == background_number_files, "foreground and background dir contains not a equal files"
+    assert len(os.listdir('./foreground')) == len(
+        os.listdir('./background')), "foreground and background dir contains not a equal number of files"
 
     for file in file_list:
         name_parts = file.split('.')
@@ -25,5 +23,9 @@ if __name__ == "__main__":
         foreground_path = './foreground/' + name_parts[0] + '.bmp'
         result_path = './results/' + name_parts[0] + '.png'
         overlay(background_path, foreground_path, result_path)
-        print('Written '+ name_parts[0] +' to '+ result_path)
+        print('Written ' + name_parts[0] + ' to ' + result_path)
+
+if __name__ == "__main__":
+    main()
+
 
